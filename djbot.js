7d533b1890fd2367289e6e4d8a293510d1d19e32
JamEngulfer <JamEngulfer221@googlemail.com>
2014-12-18 15:11:09 +0000

    Derp bracket

--- djbot.js
+++ djbot.js
@@ -21,7 +21,7 @@ piepan.On('message', function(e) {
   }
   
   console.log("Playing track: ".concat(e.Message));
-  console.log("Current bitrate: ".concat(piepan.Audio.Bitrate());
+  console.log("Current bitrate: ".concat(piepan.Audio.Bitrate()));
   piepan.Audio.SetBitrate(72000);
   piepan.Audio.Play({
       filename: directory.concat(e.Message),
